Modified it into tardiness and corrected errors in reward calculation

realtime_jsp/algorithms/Q_learning.py
@@ -34,10 +34,17 @@ class q_learning_funcs():
         of length of the action space(set of possible actions).
         """
         def policy_function(state):
+            # TO check: cz create just happens once, but action space varies, so i dont know if create_epsilon is necessary
+            num_actions = state
             action_probabilities = np.ones(num_actions, dtype=float)*self.epsilon/num_actions
-            best_action = np.argmin(Q[state])
+            # num of Q[state] can be greater than num_actions/state cz in each step, the situation can vary
+            Q_values = Q[state][:num_actions]
+            best_action = np.argmax(Q_values)
+            print("Action_prob before is ", action_probabilities)
+            print("Best_action is ", best_action)
             print("QQ ", Q)
             action_probabilities[best_action] += (1.0-self.epsilon)
+            print("Action_prob after is ", action_probabilities)
             return action_probabilities
 
         return policy_function
@@ -65,6 +72,7 @@ class q_learning_funcs():
         granularity = 1
         # For every episode
         for i_episode in range(self.num_episodes):
+            print("New Episode!!!!!!!!!!!!")
 
             # Reset the environment and pick the first action
             env.state = self.env.reset(event_simu)
@@ -76,7 +84,7 @@ class q_learning_funcs():
             # Create an epsilon greedy policy function
             # appropriately for environment action space
 
-            for t in range(20):  # itertools.count():
+            for t in range(100):  # itertools.count():
                 # Q = defaultdict(lambda: np.zeros(self.env.action_space.n))
                 # Check decision epoch according to events
                 # job release/job arrival (simulation strategy to be used?)
@@ -129,12 +137,12 @@ class q_learning_funcs():
                     # TD Update
                    # print("Test Q ", Q)
                     print(" next state ", next_state)
-                    print(" Q[next_state] is ", Q[next_state])
+                    print(" Q[next_state] is ", Q[next_state], " env_state ", env.state)
                     if next_state >= len(Q[next_state]):
                         diff = next_state - len(Q[next_state]) + 1
                         for i in range(diff):
                             Q[next_state] = np.append(Q[next_state], 0)
-                    best_next_action = np.argmin(Q[next_state])
+                    best_next_action = np.argmax(Q[next_state])
                     td_target = reward + self.discount_factor * Q[next_state][best_next_action]
                     td_delta = td_target - Q[env.state][action]
                     Q[env.state][action] += self.alpha * td_delta
@@ -154,7 +162,7 @@ if __name__ == '__main__':
     env = JSPEnv2()
     _conf = ConfigParser()
     _conf.read('./etc/app.ini')
-    num_episode =  1000
+    num_episode = 500
     #  Train the model
     Q_learn = q_learning_funcs(env, _conf, num_episode)
     Q, stats = Q_learn.q_learning(plotting)

realtime_jsp/environments/JSPEnv2.py
@@ -84,8 +84,6 @@ class JSPEnv2(gym.Env):
         # the 2nd part is 1-k
         self.price = 0.025  # price of holding one job per time unit
         self.real_num = 2.5
-        self.raw_pt = 0
-        self.remain_raw_pt = 0
 
         # TO MODIFY: use exponential distribution
         self.low = np.array([self.min_num_wait], dtype=np.float32)
@@ -124,20 +122,34 @@ class JSPEnv2(gym.Env):
         print("Job is ", self.waiting_jobs[action].to_string())
         self.machine.process_job(self.waiting_jobs[action], t)
         del self.waiting_jobs[action]
-        self.state = len(self.waiting_jobs)
+        new_state = len(self.waiting_jobs)
+        # sort jobs according to the due date, 1st one is the one with smallest due date (urgent)
+        self.waiting_jobs.sort(key=self.takeDueTime)
 
         # calculate current total tardiness as reward
         reward = 0
         for j in self.waiting_jobs:
-            reward += (t+j.pt)-j.due_t
+            tardiness = (t+j.pt)-j.due_t
+            if tardiness < 0:
+                tardiness = 0
+            reward += tardiness
+            print("At time ", t, " pt ", j.pt, " due_t ", j.due_t, " tard ", tardiness)
         processed_pt = t - self.machine.assigned_job.start_processing_t
         remained_pt = self.machine.assigned_job.pt - processed_pt
-        reward += (t+remained_pt) - self.machine.assigned_job.due_t
+        machine_job_tard = (t+remained_pt) - self.machine.assigned_job.due_t
+        if machine_job_tard < 0:
+            machine_job_tard = 0
+        reward += machine_job_tard
+        reward = -1*reward # cz this is tardiness
 
         print("Getting reward ", reward, " from action ", action)
 
-        done = bool(self.state == 0)
-        return self.state, reward, done, {}
+        done = bool(new_state == 0)
+        return new_state, reward, done, {}
+
+    def takeDueTime(self, job):
+        return job.due_t+job.pt
+
 
     def reset(self, simulator):
         # initialize machines and its job being processed
@@ -145,9 +157,6 @@ class JSPEnv2(gym.Env):
         t = 0
         machine = Machine()
         job = simulator.release_new_job(1)#self.create_jobs(1)
-        # assume the job is new, so remain_raw_pt = raw_pt
-        self.remain_raw_pt += job[0].pt
-        self.raw_pt = self.remain_raw_pt
         machine.process_job(job[0], 0)
         self.machine = machine
         # always start from 3 waiting jobs

realtime_jsp/simulators/eventsimulator2.py
@@ -8,15 +8,19 @@ class EventSimulator2:
     def __init__(self, settings):
         self.interarrival_mean_time = int(settings.get('event', 'interarrival_time'))
         self.processing_mean_time = int(settings.get('job', 'processing_time'))
+        self.min_processing_time = int(settings.get('job', 'min_processing_time'))
+        self.max_processing_time = int(settings.get('job', 'max_processing_time'))
         self.min_num_release_job = int(settings.get('job', 'min_num_release_job'))
         self.max_num_release_job = int(settings.get('job', 'max_num_release_job'))
         self.job_released = False
         self.last_release_time = 0
         self.interarrival_time = 0
-        np.random.seed(4)
+        self.current_time = 0
+        # np.random.seed(5)
 
     def event_simulation(self, t, machine, granularity):
-        to_release = self.check_job_release(t)
+        self.current_time = t
+        to_release = self.check_job_release()
         released_new_jobs = []
         if to_release:
             # number of release job
@@ -26,7 +30,7 @@ class EventSimulator2:
             # update time status
             self.interarrival_time = next_interarrival_time
             self.last_release_time = t
-        updated_machine, processed_pt = self.check_machine_idle_and_update(machine, t, granularity)
+        updated_machine, processed_pt = self.check_machine_idle_and_update(machine, granularity)
         return np.array([to_release, released_new_jobs, updated_machine, processed_pt])
 
     # release one job at a time, it can be modified later
@@ -34,14 +38,15 @@ class EventSimulator2:
         jobs = []
         for i in range(number):
             # parameter: scale = inverse of the rate parameter = mean
-            pt = int(np.random.exponential(self.processing_mean_time, 1))
-            due_t = int(np.random.exponential(2*self.processing_mean_time, 1)) + pt  # Can be modified
+            pt = np.random.randint(self.min_processing_time, self.max_processing_time)
+            # pt = int(np.random.exponential(self.processing_mean_time, 1))
+            due_t = int(np.random.exponential(self.min_processing_time, 1)) +pt+ self.current_time+1  # Can be modified
             job = JobDue(due_t, pt, -1)
             jobs.append(job)
         return jobs
 
-    def check_job_release(self, t):
-        if self.last_release_time + self.interarrival_time == t:
+    def check_job_release(self):
+        if self.last_release_time + self.interarrival_time == self.current_time:
             print("Release")
             return True
         #print("Not Release")
@@ -56,23 +61,23 @@ class EventSimulator2:
 
 
     # according to the job assigned to the machine, if job is completed, then the machine is idle
-    def check_machine_idle(self, machines, t):
+    def check_machine_idle(self, machines):
         idles = []
         for m in machines:
             if m.idle:
                 idle = True
             else:
-                idle = True if t - m.assigned_job.start_processing_pt == 0 else False
+                idle = True if self.current_time - m.assigned_job.start_processing_pt == 0 else False
                 # idle = True if m.assigned_job.remaining_pt == 0 else False
             idles.append(idle)
         return idles
 
-    def check_machine_idle_and_update(self, machine, t, granularity):
+    def check_machine_idle_and_update(self, machine, granularity):
         processed_pt = 0
         # print("In event_simu, machine num is ", len(machines))
         if machine.idle != True:
             job = machine.assigned_job
-            remained_pt = job.pt - (t - job.start_processing_t)
+            remained_pt = job.pt - (self.current_time - job.start_processing_t)
             if remained_pt <= 0:
                 processed_pt += granularity
                 machine.reset()